17e5998b8842cbf621f821933dfd7f9ebbd925d2
sg-ua <2101804954@qq.com>
2020-07-25 02:33:48 +0800

    ninth submit : correct a mistake

--- bookApp/views.py
+++ bookApp/views.py
@@ -11,7 +11,7 @@ def index(request):
     return HttpResponse("图书管理系统")
 
 def detail(request, id):
-    #book = Book.objects.get(id=id)
+    book = Book.objects.get(id=id)
     #info = """
     #书籍id： %s
     #书籍名称： %s
