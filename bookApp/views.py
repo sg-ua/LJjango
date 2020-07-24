969808bfbf9a86892cb701203d41e7674052b4ba
sg-ua <2101804954@qq.com>
2020-07-25 02:24:19 +0800

    eigth submit : complete views function and use beautiful html model to complete project, project1 finish

--- bookApp/views.py
+++ bookApp/views.py
@@ -5,13 +5,18 @@ from .models import  Book
 # Create your views here.
 #编写第一个视图函数，http协议（基于请求-响应的一个协议，
 def index(request):
+    """书籍管理的首页，显示所有的书籍信息"""
+    books = Book.objects.all()
+    return  render(request, 'bookApp/index.html', {'books':books})
     return HttpResponse("图书管理系统")
 
 def detail(request, id):
-    book = Book.objects.get(id=id)
-    info = """
-    书籍id： %s
-    书籍名称： %s
-    书籍发布日期： %s
-    """ %(book.id, book.title, book.pub_date)
-    return HttpResponse(info)
+    #book = Book.objects.get(id=id)
+    #info = """
+    #书籍id： %s
+    #书籍名称： %s
+    #书籍发布日期： %s
+    #""" %(book.id, book.title, book.pub_date)
+    #return HttpResponse(info)
+
+    return render(request, 'bookApp/detail.html', {'book': book})
